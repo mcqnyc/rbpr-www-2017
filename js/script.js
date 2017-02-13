6e77d5f2a4e9ef45efb3309bf1950765630d1a38
mcqnyc <mcqnyc@gmail.com>
2017-02-13 10:46:26 -0500

    create stick nav

--- js/script.js
+++ js/script.js
@@ -1,46 +1,33 @@
 $(document).ready(function(){
-    console.log('doc is ready');
+
     /* For the sticky navigation */
     $('.js--services-section').waypoint(function(direction){
-      console.log('iiiin waypoint');
-        if(direction === "down"){
-            $('nav').addClass('sticky-header-nav');
-            $('nav>a>div').addClass('rbpr-nav-name');
-            $('nav>ul>li').addClass('sticky-header-nav');
-
-        } else {
-            $('nav').removeClass('sticky-header-nav');
-        }
+      if(direction === "down"){
+          $('.sticky-header-nav').css('opacity', 1)
+      } else {
+          $('.sticky-header-nav').css('opacity', 0)
+      }
     }, {
-      offset: 1em;
+      offset: '3em'
     });
-   
-    /* Scroll on buttons */
-    // $('.js--scroll-to-plans').click(function(){
-    //    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
-    // });
-    
-    // $('.js--scroll-to-start').click(function(){
-    //    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
-    // });
     
     
     /* Navigation scroll */
     
-    // $(function() {
-    //   $('a[href*="#"]:not([href="#"])').click(function() {
-    //     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-    //       var target = $(this.hash);
-    //       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    //       if (target.length) {
-    //         $('html, body').animate({
-    //           scrollTop: target.offset().top
-    //         }, 1000);
-    //         return false;
-    //       }
-    //     }
-    //   });
-    // });
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 700);
+            return false;
+          }
+        }
+      });
+    });
     
     /* Animate on scroll */
     $('.js--fade-in').waypoint(function(direction){
